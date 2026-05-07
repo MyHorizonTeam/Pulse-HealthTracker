eeb4817beb25a3dbf737745d7d216d9820642253
Niro <sachithananthan89@gmail.com>
2026-05-07 16:44:46 +0530

    chore:updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application) // Since this here we don't need to add "id("com.android.application")"
-    alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
 }
 
