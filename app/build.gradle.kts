32998288018b1b2ce2c617d15f8dd63be4e63b0c
musharraf-mac <musharrafcm97@outlook.com>
2026-05-20 07:39:23 +0530

    feat:switch for toggle between register & login has been implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-auth")
-
+    implementation("com.firebaseui:firebase-ui-auth:9.1.1")
     implementation("androidx.credentials:credentials:1.3.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
--- app/src/main/java/com/example/pulse_healthtracker/Login.kt
+++ app/src/main/java/com/example/pulse_healthtracker/Login.kt
@@ -7,23 +7,73 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.*
 import android.content.Intent
-
+import android.view.View
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.button.MaterialButtonToggleGroup
 class Login : AppCompatActivity() {
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract(),
+    ) { res ->
+        this.onSignInResult(res)
+    }
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
-        val regSwitch=findViewById<Button>(R.id.regBtn)
-        regSwitch.setOnClickListener{
-            val intent = Intent(this,Register::class.java )
-        }
+
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+        )
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
         var email_u = findViewById<EditText>(R.id.edtxtEmail)
         var pwd = findViewById<EditText>(R.id.edtxtPwd)
         val lgBtn = findViewById<Button>(R.id.btnLog)
         val frgpwd = findViewById<TextView>(R.id.frgpwd)
+        val togglegrp = findViewById<MaterialButtonToggleGroup>(R.id.authToggleGroup)
+        val logLyt = findViewById<LinearLayout>(R.id.logLyt)
+        val regLyt = findViewById<LinearLayout>(R.id.regLyt)
 
-
+        togglegrp.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked){
+                when (checkedId) {
+                    R.id.logswitch->{
+                        logLyt.visibility = View.VISIBLE
+                        regLyt.visibility = View.GONE
+                    }
+                    R.id.regswitch ->{
+                        logLyt.visibility = View.GONE
+                        regLyt.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
