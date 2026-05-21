e222c54cad6f091e66114612be3b2f826d01427b
musharraf-mac <musharrafcm97@outlook.com>
2026-05-21 07:24:57 +0530

    Just some updates

--- app/src/main/java/com/example/pulse_healthtracker/Login.kt
+++ app/src/main/java/com/example/pulse_healthtracker/Login.kt
@@ -14,7 +14,8 @@ import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.android.material.button.MaterialButtonToggleGroup
-class Login : AppCompatActivity() {
+import com.google.firebase.auth.GoogleAuthProvider
+abstract class Login : AppCompatActivity() {
 
     // See: https://developer.android.com/training/basics/intents/result
     private val signInLauncher = registerForActivityResult(
@@ -22,36 +23,76 @@ class Login : AppCompatActivity() {
     ) { res ->
         this.onSignInResult(res)
     }
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        togglegrp.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.logswitch -> {
+                        logLyt.visibility = View.VISIBLE
+                        regLyt.visibility = View.GONE
+                    }
+
+                    R.id.regswitch -> {
+                        logLyt.visibility = View.GONE
+                        regLyt.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
 
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+    private fun createSignInIntent() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build(),
         )
         // Create and launch sign-in intent
+        /*btnGoogleSignIn.setOnClickListener {
         val signInIntent = AuthUI.getInstance()
             .createSignInIntentBuilder()
             .setAvailableProviders(providers)
             .build()
         signInLauncher.launch(signInIntent)
+    }*/
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            Toast.makeText(this, "Succesfully signed in", Toast.LENGTH_SHORT).show()
+            // ...
+        } else {
+            Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // ...
+            }
+        // [END auth_fui_signout]
+    }
         var email_u = findViewById<EditText>(R.id.edtxtEmail)
         var pwd = findViewById<EditText>(R.id.edtxtPwd)
         val lgBtn = findViewById<Button>(R.id.btnLog)
@@ -60,25 +101,6 @@ class Login : AppCompatActivity() {
         val logLyt = findViewById<LinearLayout>(R.id.logLyt)
         val regLyt = findViewById<LinearLayout>(R.id.regLyt)
 
-        togglegrp.addOnButtonCheckedListener { group, checkedId, isChecked ->
-            if (isChecked){
-                when (checkedId) {
-                    R.id.logswitch->{
-                        logLyt.visibility = View.VISIBLE
-                        regLyt.visibility = View.GONE
-                    }
-                    R.id.regswitch ->{
-                        logLyt.visibility = View.GONE
-                        regLyt.visibility = View.VISIBLE
-                    }
-                }
-            }
+
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
