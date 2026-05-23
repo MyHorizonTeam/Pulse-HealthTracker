429ef0455734a3450189e30e148d6747aa42e865
musharraf-mac <musharrafcm97@outlook.com>
2026-05-23 08:06:02 +0530

    Feat:Logout button added to profile_pg.kt

--- app/src/main/java/com/example/pulse_healthtracker/profile_pg.kt
+++ app/src/main/java/com/example/pulse_healthtracker/profile_pg.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.firebase.ui.auth.AuthUI
+import android.widget.*
+import android.content.Intent
 class profile_pg : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_pg)
+        var lgOut = findViewById<Button>(R.id.lgOut)
+        lgOut.setOnClickListener {
+            signOut()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private fun signOut(){
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+                finish()
+            }
     }
 }
