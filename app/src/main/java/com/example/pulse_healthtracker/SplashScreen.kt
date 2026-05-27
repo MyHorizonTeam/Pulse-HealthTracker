c263e3dbe660b5ded7ef730d69701bda1b48677e
Mactavi5h <MUSHARRAFcm97@outlook.com>
2026-05-27 06:23:57 +0530

    Splash screen adjused

--- app/src/main/java/com/example/pulse_healthtracker/SplashScreen.kt
+++ app/src/main/java/com/example/pulse_healthtracker/SplashScreen.kt
@@ -5,12 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
+        enableEdgeToEdge()
+
+        val btn_getS = findViewById<Button>(R.id.btn_getS)
+        btn_getS.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
