541fed46a9e4b23ceed69695bf8e08945b35005d
Mactavi5h <MUSHARRAFcm97@outlook.com>
2026-05-30 01:58:31 +0530

    feat:CRUD function for medication mgt has been added

---
+++ app/src/main/java/com/example/pulse_healthtracker/Medicine.kt
@@ -0,0 +1,21 @@
+package com.example.pulse_healthtracker
+
+import com.google.firebase.firestore.PropertyName
+
+data class Medicine(
+    var medicineId: String = "",
+    var pillName: String = "",
+    var diseases: String = "",
+    var dose: Double = 0.0,
+    var time: String = "",
+    var foodRelation: String = "",
+    var notes: String = "",
+    var medicineTimes: List<String> = emptyList(),
+    var createdAt: Long = 0,
+    var status: String = "active",
+    var userId: String = "",
+    
+    @get:PropertyName("isTaken")
+    @set:PropertyName("isTaken")
+    var isTaken: Boolean = false
+)
