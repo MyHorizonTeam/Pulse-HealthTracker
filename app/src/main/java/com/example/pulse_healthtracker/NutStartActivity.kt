b551ae3c3cda4a32016d01b426c2c6bf28cdd40d
dilka fernando <dilkaraviharee99@gmail.com>
2026-05-29 15:48:01 +0530

    add xml files

--- app/src/main/java/com/example/pulse_healthtracker/NutStartActivity.kt
+++ app/src/main/java/com/example/pulse_healthtracker/NutStartActivity.kt
@@ -5,37 +5,25 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class NutStartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_nut_start)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_nut_start)
 
         val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
         val txtLoginLink = findViewById<TextView>(R.id.txtLoginLink)
 
-
         btnGetStarted.setOnClickListener {
             Toast.makeText(this, "Redirecting to Dashboard...", Toast.LENGTH_SHORT).show()
-
 
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
             finish()
         }
-
 
         txtLoginLink.setOnClickListener {
             Toast.makeText(this, "Redirecting to Login...", Toast.LENGTH_SHORT).show()
